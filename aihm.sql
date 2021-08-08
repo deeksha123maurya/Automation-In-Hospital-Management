ccd959117d6c9fb944ad5d2f6fc691c525dd0af3
deeksha123maurya <88630954+deeksha123maurya@users.noreply.github.com>
2021-08-09 00:58:42 +0530

    Add files via upload

---
+++ aihm.sql
@@ -0,0 +1,290 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 30, 2020 at 12:48 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aihm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adminlogin_tb`
+--
+
+CREATE TABLE `adminlogin_tb` (
+  `a_login_id` int(11) NOT NULL,
+  `a_name` varchar(60) COLLATE utf8_bin NOT NULL,
+  `a_email` varchar(60) COLLATE utf8_bin NOT NULL,
+  `a_password` varchar(60) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `adminlogin_tb`
+--
+
+INSERT INTO `adminlogin_tb` (`a_login_id`, `a_name`, `a_email`, `a_password`) VALUES
+(1, 'Admin', 'admin@aihm.com', '123456'),
+(2, 'Admin', 'admin@aihm.com', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assets_tb`
+--
+
+CREATE TABLE `assets_tb` (
+  `pid` int(11) NOT NULL,
+  `pname` varchar(60) COLLATE utf8_bin NOT NULL,
+  `pdop` date NOT NULL,
+  `pava` int(11) NOT NULL,
+  `ptotal` int(11) NOT NULL,
+  `poriginalcost` int(11) NOT NULL,
+  `psellingcost` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `assets_tb`
+--
+
+INSERT INTO `assets_tb` (`pid`, `pname`, `pdop`, `pava`, `ptotal`, `poriginalcost`, `psellingcost`) VALUES
+(1, 'fffff', '2020-09-01', 0, 3, 5, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assigndoctor_tb`
+--
+
+CREATE TABLE `assigndoctor_tb` (
+  `mo` int(11) NOT NULL,
+  `request_id` int(11) NOT NULL,
+  `request_info` text COLLATE utf8_bin NOT NULL,
+  `request_desc` text COLLATE utf8_bin NOT NULL,
+  `requester_name` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_add1` text COLLATE utf8_bin NOT NULL,
+  `requester_add2` text COLLATE utf8_bin NOT NULL,
+  `requester_city` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_state` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_zip` int(11) NOT NULL,
+  `requester_email` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_mobile` bigint(20) NOT NULL,
+  `assign_doctor` varchar(60) COLLATE utf8_bin NOT NULL,
+  `assign_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `assigndoctor_tb`
+--
+
+INSERT INTO `assigndoctor_tb` (`mo`, `request_id`, `request_info`, `request_desc`, `requester_name`, `requester_add1`, `requester_add2`, `requester_city`, `requester_state`, `requester_zip`, `requester_email`, `requester_mobile`, `assign_doctor`, `assign_date`) VALUES
+(1, 1, 'I am not ok', 'i am nit ok', 'FFFFF', 'MNNIT staff colony', 'Railway', 'Prayagraj', 'Uttar Pradesh', 211004, 'fff@gmail.com', 2244224422, 'Zahir Khan', '2020-09-26'),
+(2, 1, 'I am not ok', 'i am nit ok', 'FFFFF', 'MNNIT staff colony', 'Railway', 'Prayagraj', 'Uttar Pradesh', 211004, 'fff@gmail.com', 2244224422, 'Zahir Khan', '2020-09-25'),
+(3, 2, 'I am not ok', 'i am nit ok', 'Deeksha', 'MNNIT staff colony', 'Railway', 'Prayagraj', 'Uttar Pradesh', 211004, 'dddd@aihm.com', 2244224422, 'Sita Kapoor', '2020-09-24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_tb`
+--
+
+CREATE TABLE `customer_tb` (
+  `custid` int(11) NOT NULL,
+  `custname` varchar(60) COLLATE utf8_bin NOT NULL,
+  `custadd` varchar(60) COLLATE utf8_bin NOT NULL,
+  `cpname` varchar(60) COLLATE utf8_bin NOT NULL,
+  `cpquantity` int(11) NOT NULL,
+  `cpeach` int(11) NOT NULL,
+  `cptotal` int(11) NOT NULL,
+  `cpdate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `customer_tb`
+--
+
+INSERT INTO `customer_tb` (`custid`, `custname`, `custadd`, `cpname`, `cpquantity`, `cpeach`, `cptotal`, `cpdate`) VALUES
+(1, 'Deeksha', 'MNNIT staff colony', 'fffff', 6, 7, 6, '2020-09-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor_tb`
+--
+
+CREATE TABLE `doctor_tb` (
+  `empid` int(11) NOT NULL,
+  `empName` varchar(60) COLLATE utf8_bin NOT NULL,
+  `empCity` varchar(60) COLLATE utf8_bin NOT NULL,
+  `empMobile` bigint(20) NOT NULL,
+  `empEmail` varchar(60) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `doctor_tb`
+--
+
+INSERT INTO `doctor_tb` (`empid`, `empName`, `empCity`, `empMobile`, `empEmail`) VALUES
+(1, 'Seeta', 'ddddd', 1234445, 'seeta@aihm.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requesterlogin_tb`
+--
+
+CREATE TABLE `requesterlogin_tb` (
+  `r_login_id` int(11) NOT NULL,
+  `r_name` varchar(60) COLLATE utf8_bin NOT NULL,
+  `r_email` varchar(60) COLLATE utf8_bin NOT NULL,
+  `r_password` varchar(60) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `requesterlogin_tb`
+--
+
+INSERT INTO `requesterlogin_tb` (`r_login_id`, `r_name`, `r_email`, `r_password`) VALUES
+(1, ' FFFFF', 'fff@gmail.com', '123456'),
+(2, 'Deeksha', 'dddd@aihm.com', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `submitrequest_tb`
+--
+
+CREATE TABLE `submitrequest_tb` (
+  `request_id` int(11) NOT NULL,
+  `request_info` text COLLATE utf8_bin NOT NULL,
+  `request_desc` text COLLATE utf8_bin NOT NULL,
+  `requester_name` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_add1` text COLLATE utf8_bin NOT NULL,
+  `requester_add2` text COLLATE utf8_bin NOT NULL,
+  `requester_city` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_state` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_zip` int(11) NOT NULL,
+  `requester_email` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_mobile` bigint(20) NOT NULL,
+  `request_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `submitrequest_tb`
+--
+
+INSERT INTO `submitrequest_tb` (`request_id`, `request_info`, `request_desc`, `requester_name`, `requester_add1`, `requester_add2`, `requester_city`, `requester_state`, `requester_zip`, `requester_email`, `requester_mobile`, `request_date`) VALUES
+(1, 'I am not ok', 'i am nit ok', 'FFFFF', 'MNNIT staff colony', 'Railway', 'Prayagraj', 'Uttar Pradesh', 211004, 'fff@gmail.com', 2244224422, '2020-09-27'),
+(2, 'I am not ok', 'i am nit ok', 'Deeksha', 'MNNIT staff colony', 'Railway', 'Prayagraj', 'Uttar Pradesh', 211004, 'dddd@aihm.com', 2244224422, '2020-09-27');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `adminlogin_tb`
+--
+ALTER TABLE `adminlogin_tb`
+  ADD PRIMARY KEY (`a_login_id`);
+
+--
+-- Indexes for table `assets_tb`
+--
+ALTER TABLE `assets_tb`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `assigndoctor_tb`
+--
+ALTER TABLE `assigndoctor_tb`
+  ADD PRIMARY KEY (`mo`);
+
+--
+-- Indexes for table `customer_tb`
+--
+ALTER TABLE `customer_tb`
+  ADD PRIMARY KEY (`custid`);
+
+--
+-- Indexes for table `doctor_tb`
+--
+ALTER TABLE `doctor_tb`
+  ADD PRIMARY KEY (`empid`);
+
+--
+-- Indexes for table `requesterlogin_tb`
+--
+ALTER TABLE `requesterlogin_tb`
+  ADD PRIMARY KEY (`r_login_id`);
+
+--
+-- Indexes for table `submitrequest_tb`
+--
+ALTER TABLE `submitrequest_tb`
+  ADD PRIMARY KEY (`request_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `adminlogin_tb`
+--
+ALTER TABLE `adminlogin_tb`
+  MODIFY `a_login_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `assets_tb`
+--
+ALTER TABLE `assets_tb`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `assigndoctor_tb`
+--
+ALTER TABLE `assigndoctor_tb`
+  MODIFY `mo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `customer_tb`
+--
+ALTER TABLE `customer_tb`
+  MODIFY `custid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `doctor_tb`
+--
+ALTER TABLE `doctor_tb`
+  MODIFY `empid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `requesterlogin_tb`
+--
+ALTER TABLE `requesterlogin_tb`
+  MODIFY `r_login_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `submitrequest_tb`
+--
+ALTER TABLE `submitrequest_tb`
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
